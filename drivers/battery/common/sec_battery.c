97c1ac3c265e8a59028e6abb04978b6ad69b0065
Andreas Schneider <asn@cryptomilk.org>
2021-01-18 13:35:11 +0100

    drivers: battery_v2: sec_battery: fix multiplication factor for input voltage
    Change-Id: I3c8ac04bc7f4bd8be7d6e6447388b6fb59e045fe

--- drivers/battery/common/sec_battery.c
+++ drivers/battery/common/sec_battery.c
@@ -5185,7 +5185,7 @@ static int sec_usb_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_MAX:
 		/* V -> uV */
-		val->intval = battery->input_voltage * 1000000;
+		val->intval = battery->input_voltage * 100000;
 		return 0;
 	case POWER_SUPPLY_PROP_CURRENT_MAX:
 		/* mA -> uA */
@@ -5272,7 +5272,7 @@ static int sec_ac_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_MAX:
 		/* V -> uV */
-		val->intval = battery->input_voltage * 1000000;
+		val->intval = battery->input_voltage * 100000;
 		return 0;
 	case POWER_SUPPLY_PROP_CURRENT_MAX:
 		/* mA -> uA */
@@ -5336,7 +5336,7 @@ static int sec_wireless_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_VOLTAGE_MAX:
 		/* V -> uV */
-		val->intval = battery->input_voltage * 1000000;
+		val->intval = battery->input_voltage * 100000;
 		return 0;
 	case POWER_SUPPLY_PROP_CURRENT_MAX:
 		/* mA -> uA */
